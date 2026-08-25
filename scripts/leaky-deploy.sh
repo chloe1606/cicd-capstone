4425c63efe170bbc51f6874f8de487c85ac5c9c8
Chloe Haffenden <chaffenden@icloud.com>
2026-08-25 13:21:58 +0100

    updates to secrets

--- scripts/leaky-deploy.sh
+++ scripts/leaky-deploy.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-ENV=$1
-if [ "$ENV" = "dev" ]; then
-  PASSWORD=$DEV_DB_PASSWORD
-elif [ "$ENV" = "uat" ]; then
-  PASSWORD=$UAT_DB_PASSWORD
-elif [ "$ENV" = "prod" ]; then
-  PASSWORD=$PROD_DB_PASSWORD
-fi
 ./deploy --password "$PASSWORD"
